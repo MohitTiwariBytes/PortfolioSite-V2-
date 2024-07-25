830e0a988a385e6a5b09f168cfcb982145b28071
mohittiwariis97 <mohittiwariis97@gmail.com>
2024-07-25 12:44:30 +0000

    COOOOKED!! IntroPage completed!

---
+++ src/App.jsx
@@ -0,0 +1,78 @@
+import "./App.css";
+import gsap from "gsap";
+import { useEffect } from "react";
+
+function App() {
+  useEffect(() => {
+    const h1Elements = document.querySelectorAll(".item h1");
+
+    function animateFromTop() {
+      const introPage = document.getElementById("intro-page");
+      let tl = gsap.timeline({ ease: "power4.inOut" });
+
+      tl.set(".transition-container .tile", { pointerEvents: "none" });
+      tl.to(".transition-container .tile", {
+        duration: 0.4,
+        height: "100%",
+        top: "0%",
+        delay: 0.2,
+        stagger: 0.05,
+      });
+      tl.to(".transition-container .tile", {
+        duration: 0.4,
+        height: "100%",
+        top: "100%",
+        delay: 0.2,
+        stagger: -0.05,
+      });
+      tl.set(".transition-container .tile", { top: "0", height: "0" });
+      tl.set(".transition-container .tile", { pointerEvents: "all" });
+      tl.set(introPage, {
+        y: 1000,
+        duration: 0.6,
+        ease: "power1.inOut",
+        onComplete: () => (introPage.style.display = "none"),
+      });
+    }
+
+    const timeline = gsap.timeline();
+
+    h1Elements.forEach((h1, index) => {
+      timeline
+        .set(h1, { opacity: 0, visibility: "hidden" })
+        .to(h1, { opacity: 1, visibility: "visible", duration: 0 })
+        .to(h1, { duration: 0.2 })
+        .to(h1, { opacity: 0, visibility: "hidden", duration: 0 });
+    });
+
+    timeline.add(() => animateFromTop(), `+=0.3`);
+  }, []);
+
+  return (
+    <div className="main-home-page">
+      <div className="home-page">
+        <div id="intro-page" className="intro-page">
+          <div className="transition-container from-top">
+            <span className="tile"></span>
+            <span className="tile"></span>
+            <span className="tile"></span>
+            <span className="tile"></span>
+            <span className="tile"></span>
+            <span className="tile"></span>
+          </div>
+          <div className="item">
+            <h1>Hello!</h1>
+            <h1>नमस्ते</h1>
+            <h1>こんにちは</h1>
+            <h1>Hola!</h1>
+            <h1>你好</h1>
+            <h1>안녕하세요</h1>
+            <h1>Ciao!</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
