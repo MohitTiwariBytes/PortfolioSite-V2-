5c5ebcca38cf9f05775435ebd6f9b7190798ac0d
mohittiwariis97 <mohittiwariis97@gmail.com>
2024-07-25 16:04:57 +0000

    Some changes on the front page!

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import myImage from "./assets/myImage.png";
 function App() {
   useEffect(() => {
     const h1Elements = document.querySelectorAll(".item h1");
+    const wrapperH1Elements = document.querySelectorAll(".wrapper h1");
 
     function animateFromTop() {
       const introPage = document.getElementById("intro-page");
@@ -30,18 +31,32 @@ function App() {
       tl.set(".transition-container .tile", { top: "0", height: "0" });
       tl.set(".transition-container .tile", { pointerEvents: "all" });
 
-      // Animate width to 0 and then set display to none
       tl.to(introPage, {
         duration: 0.5,
         height: "0%",
         onComplete: () => {
           introPage.style.display = "none";
+          gsap.fromTo(
+            wrapperH1Elements,
+            {
+              top: "1000px",
+              y: "1700px",
+            },
+            {
+              top: "0px",
+              y: "0px",
+              duration: 1,
+              ease: "power4.out",
+              stagger: 0.1,
+            }
+          );
         },
       });
     }
 
     const timeline = gsap.timeline();
 
+    // Animation for h1 elements in .item
     h1Elements.forEach((h1, index) => {
       timeline
         .set(h1, { opacity: 0, visibility: "hidden" })
@@ -49,7 +64,6 @@ function App() {
         .to(h1, { duration: 0.2 })
         .to(h1, { opacity: 0, visibility: "hidden", duration: 0 });
     });
-
     timeline.add(() => animateFromTop(), `+=0.3`);
   }, []);
 
@@ -78,9 +92,23 @@ function App() {
 
         <div id="main-page" className="main-page">
           <div className="textBackground">
-            <h1>Mohit</h1>
-            <h1>Tiwari</h1>
-            <h1>Dev</h1>
+            <div className="image">
+              <img
+                style={{ position: "absolute", zIndex: "100" }}
+                height={"800px"}
+                src={myImage}
+                alt=""
+              />
+            </div>
+            <div className="wrapper">
+              <h1>Mohit</h1>
+            </div>
+            <div className="wrapper">
+              <h1>Tiwari</h1>
+            </div>
+            <div className="wrapper">
+              <h1>Dev</h1>
+            </div>
           </div>
         </div>
       </div>
