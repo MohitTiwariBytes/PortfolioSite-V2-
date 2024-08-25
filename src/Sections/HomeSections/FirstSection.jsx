32e9aa07095c603b6d27ffeb5ca3eb299ac2777d
mohittiwariis97 <mohittiwariis97@gmail.com>
2024-08-25 13:49:44 +0000

    Some enchancement in the site

--- src/Sections/HomeSections/FirstSection.jsx
+++ src/Sections/HomeSections/FirstSection.jsx
@@ -4,44 +4,25 @@ import "./FirstSection.css";
 
 const FirstSection = () => {
   useEffect(() => {
-    gsap.fromTo(
-      ".text-main h1",
-      {
-        y: "1700px",
-      },
-      {
-        y: "0px",
-        duration: 1.1,
-        ease: "power4.out",
-        stagger: 0.1,
-      }
-    );
+    const initAnimations = () => {
+      gsap.fromTo(
+        ".text-main h1",
+        { y: "1700px" },
+        { y: "0px", duration: 1.1, ease: "power4.out" }
+      );
+      gsap.fromTo(
+        ".frontend p",
+        { y: "1700px" },
+        { y: "0px", duration: 1.1, ease: "power4.out" }
+      );
+      gsap.fromTo(
+        ".location p",
+        { y: "1700px" },
+        { y: "0px", duration: 1.1, ease: "power4.out" }
+      );
+    };
 
-    gsap.fromTo(
-      ".frontend p",
-      {
-        y: "1700px",
-      },
-      {
-        y: "0px",
-        duration: 1.1,
-        ease: "power4.out",
-        stagger: 0.1,
-      }
-    );
-
-    gsap.fromTo(
-      ".location p",
-      {
-        y: "1700px",
-      },
-      {
-        y: "0px",
-        duration: 1.1,
-        ease: "power4.out",
-        stagger: 0.1,
-      }
-    );
+    initAnimations();
   }, []);
 
   return (
@@ -49,10 +30,9 @@ const FirstSection = () => {
       <div className="first-section">
         <div className="front-text">
           <div className="text-main">
-            <h1>MohitTiwariDev</h1>
+            <h1 id="front-text">MohitTiwariDev</h1>
           </div>
         </div>
-
         <div className="bottom">
           <div className="frontend">
             <p>13 yo front-end dev</p>
--- src/Sections/HomeSections/ThirdSection.jsx
+++ src/Sections/HomeSections/ThirdSection.jsx
@@ -2,44 +2,7 @@ import React from "react";
 import "./ThirdSection.css";
 
 const ThirdSection = () => {
-  return (
-    <div className="third-section-main">
-      <div className="third-section">
-        <div className="topProjects">
-          <h1>Projects.</h1>
-        </div>
-        <div className="projects">
-          <div className="p1 p">
-            <div className="name">
-              <h1>Foodzy!</h1>
-            </div>
-            <div className="info">
-              <span>An app that finds recipe for you!</span>
-            </div>
-          </div>
-          <div className="p2 p">
-            <div className="name">
-              <h1>TeenCodeHub</h1>
-            </div>
-            <div className="info">
-              <span>An community app for teens!</span>
-            </div>
-          </div>
-          <div className="p3 p">
-            <div className="name">
-              <h1>TheUnclickableButton</h1>
-            </div>
-            <div className="info">
-              <span>
-                A game where you'll get a button and then you have to click on
-                that button, You'll find out the twist
-              </span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return 0;
 };
 
 export default ThirdSection;
